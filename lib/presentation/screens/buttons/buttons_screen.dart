da9f444fd656ca6939ce532063a84dee4dd2322c
Alan Barrera <aalan.eduardo@gmail.com>
2023-06-20 22:19:44 -0600

    buttons scren finished

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -64,6 +64,8 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon( Icons.account_box_rounded ),
               label: const Text( 'Text Icon' ) ),
 
+              const CustomButton(),
+
             IconButton(
               onPressed: (){},
               icon: const Icon( Icons.app_registration_outlined ), ),
@@ -79,6 +81,32 @@ class _ButtonsView extends StatelessWidget {
     
           ],
     
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular( 20 ),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {
+          },
+          child: const Padding(
+            padding: EdgeInsets.symmetric( horizontal: 20, vertical: 10 ),
+            child: Text( 'Hola Mundo', style: TextStyle( color: Colors.white ), ),
+          ),
         ),
       ),
     );
