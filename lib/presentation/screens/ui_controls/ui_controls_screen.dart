1bec9b6470e25daf45fc8f37029d20e0f1acda31
Alan Barrera <aalan.eduardo@gmail.com>
2023-06-23 23:18:04 -0600

    UI Screen finished

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -33,6 +33,9 @@ class _UiControlsViewState extends State<_UiControlsView> {
 
   bool isDeveloper = true;
   Transportation selectedTransportation = Transportation.car;
+  bool wantsBreakfast = false;
+  bool wantsLunch     = false;
+  bool wantsDinner    = false;
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +51,11 @@ class _UiControlsViewState extends State<_UiControlsView> {
           }) )
         ),
 
-        RadioListTile(
+        ExpansionTile(
+          title: const Text('Transport Vehicles'),
+          subtitle: Text('$selectedTransportation'),
+          children: [
+            RadioListTile(
           title: const Text('By Car'),
           subtitle: const Text('Trip by car'),
           value: Transportation.car,
@@ -92,6 +99,31 @@ class _UiControlsViewState extends State<_UiControlsView> {
 
           })
         ),
+          ],
+        ),
+
+        CheckboxListTile(
+          title: const Text('¿Breakfast?'),
+          value: wantsBreakfast,
+          onChanged: ( value ) =>  setState(() {
+            wantsBreakfast = !wantsBreakfast;
+          })
+        ),
+
+        CheckboxListTile(
+          title: const Text('Lunch?'),
+          value: wantsLunch,
+          onChanged: ( value ) => setState(() {
+            wantsLunch = !wantsLunch;
+          })
+        ),
+
+        CheckboxListTile(
+          title: const Text('Dinner?'),
+          value: wantsDinner,
+          onChanged: ( value ) =>   setState(() {
+            wantsDinner = !wantsDinner;
+          }))
 
       ],
     );
